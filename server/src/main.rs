0671e04ac0ea82151d45e5e6baf3aa935b063e3e
Lars Francke <lars.francke@gmail.com>
2021-05-27 09:15:02 +0200

    Add a log line

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,7 @@ use stackable_operator::{client, error};
 async fn main() -> Result<(), error::Error> {
     stackable_operator::logging::initialize_logging("NIFI_OPERATOR_LOG");
 
+    info!("Starting Stackable Operator for Apache NiFi");
     let client = client::create_client(Some("nifi.stackable.tech".to_string())).await?;
     stackable_nifi_operator::create_controller(client).await;
     Ok(())
