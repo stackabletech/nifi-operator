b52a339818de9068221c3ae517ac8c69983a9a31
maltesander <malte.sander.it@gmail.com>
2021-08-31 12:31:13 +0200

    Product updates, CLI handling, change log.

--- crd/src/bin/generate_crds.rs
+++
@@ -1,8 +0,0 @@
-use stackable_nifi_crd::NifiCluster;
-use stackable_operator::crd::CustomResourceExt;
-
-fn main() {
-    let target_file = "deploy/crd/nificluster.crd.yaml";
-    NifiCluster::write_yaml_schema(target_file).unwrap();
-    println!("Wrote CRD to [{}]", target_file);
-}
