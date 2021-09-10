fc0220ff3284c1e636cdd0202eced822d5e0fcbd
Sönke Liebau <soenke.liebau@stackable.de>
2021-09-10 21:50:58 +0200

    Refactored structure of operator. (#72)
    This PR addresses a couple of issues with the general structure of the operator and CI workflows.
    
    All rust crates are moved to the /rust directory
    The -server crate is renamed to -binary
    The binary for this crate is stripped of the -binary suffix
    A workflow is added that runs for any new tag in this repo and pushes resulting artifacts to the release repo
    cargo parameters in rust workflows have been added to warn of incompatibilities with the next rust version
    cargo parameters to speed up the build have been added to rust workflows

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -4,7 +4,7 @@ use stackable_operator::crd::CustomResourceExt;
 fn main() -> Result<(), stackable_operator::error::Error> {
     built::write_built_file().expect("Failed to acquire build-time information");
 
-    NifiCluster::write_yaml_schema("../deploy/crd/nificluster.crd.yaml")?;
+    NifiCluster::write_yaml_schema("../../deploy/crd/nificluster.crd.yaml")?;
 
     Ok(())
 }
