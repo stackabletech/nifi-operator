aab75f3abdef0cd278e30c70ccc06584b575c69f
Nikolaus Winter <mail@nikolaus-winter.de>
2022-06-21 15:48:31 +0200

    removed CI files

--- .ci/integration-tests/ionos-k8s/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone -b "$GIT_BRANCH" https://github.com/stackabletech/nifi-operator.git
-(cd nifi-operator/ && ./scripts/run_tests.sh --parallel 1)
-exit_code=$?
-./operator-logs.sh nifi > /target/nifi-operator.log
-./operator-logs.sh zookeeper > /target/zookeeper-operator.log
-exit $exit_code
