4ebaf348ca35f68bfb53b3d4ee5b3b953014d537
Malte Sander <contact@maltesander.com>
2023-01-10 11:40:59 +0000

    Removed deploy crd folder (#405)
    # Description
    
    fixes https://github.com/stackabletech/nifi-operator/issues/232
    
    
    
    Co-authored-by: Malte Sander <malte.sander.it@gmail.com>

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,10 +1,3 @@
-use stackable_nifi_crd::NifiCluster;
-use stackable_operator::crd::CustomResourceExt;
-
-fn main() -> Result<(), stackable_operator::error::Error> {
+fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    NifiCluster::write_yaml_schema("../../deploy/crd/nificluster.crd.yaml")?;
-
-    Ok(())
 }
