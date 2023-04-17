7cd36454a25b97e0d8b8a5bf8d288586ab509cf7
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2023-04-17 11:08:37 +0200

    release 23.4.0

--- docs/modules/nifi/examples/getting_started/getting_started.sh
+++ docs/modules/nifi/examples/getting_started/getting_started.sh
@@ -21,29 +21,29 @@ cd "$(dirname "$0")"
 
 case "$1" in
 "helm")
-echo "Adding 'stackable-dev' Helm Chart repository"
+echo "Adding 'stackable-stable' Helm Chart repository"
 # tag::helm-add-repo[]
-helm repo add stackable-dev https://repo.stackable.tech/repository/helm-dev/
+helm repo add stackable-stable https://repo.stackable.tech/repository/helm-stable/
 # end::helm-add-repo[]
 echo "Updating Helm repo"
 helm repo update
 
 echo "Installing Operators with Helm"
 # tag::helm-install-operators[]
-helm install --wait commons-operator stackable-dev/commons-operator --version 0.0.0-dev
-helm install --wait secret-operator stackable-dev/secret-operator --version 0.0.0-dev
-helm install --wait zookeeper-operator stackable-dev/zookeeper-operator --version 0.0.0-dev
-helm install --wait nifi-operator stackable-dev/nifi-operator --version 0.0.0-dev
+helm install --wait commons-operator stackable-stable/commons-operator --version 23.4.0
+helm install --wait secret-operator stackable-stable/secret-operator --version 23.4.0
+helm install --wait zookeeper-operator stackable-stable/zookeeper-operator --version 23.4.0
+helm install --wait nifi-operator stackable-stable/nifi-operator --version 23.4.0
 # end::helm-install-operators[]
 ;;
 "stackablectl")
 echo "installing Operators with stackablectl"
 # tag::stackablectl-install-operators[]
 stackablectl operator install \
-  commons=0.0.0-dev \
-  secret=0.0.0-dev \
-  zookeeper=0.0.0-dev \
-  nifi=0.0.0-dev
+  commons=23.4.0 \
+  secret=23.4.0 \
+  zookeeper=23.4.0 \
+  nifi=23.4.0
 # end::stackablectl-install-operators[]
 ;;
 *)
