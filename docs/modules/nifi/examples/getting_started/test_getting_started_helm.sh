3af84ff52d597f8c294053010a4f8777ac4d7fd5
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2023-11-03 09:15:22 +0100

    commented out ExternalIP from hel callout as the json parsing does not work on e.g. kind

--- docs/modules/nifi/examples/getting_started/test_getting_started_helm.sh
+++ docs/modules/nifi/examples/getting_started/test_getting_started_helm.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 cd "$(dirname "$0")"
-./getting_started.sh helm ExternalIP
+./getting_started.sh helm #ExternalIP commenting this out as the json parsing does not work with e.g. Kind
 
