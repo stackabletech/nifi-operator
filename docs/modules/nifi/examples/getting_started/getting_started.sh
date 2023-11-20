ee06e0bdef7994c7b7c6ace6bcd20884b50488d6
Siegfried Weber <mail@siegfriedweber.net>
2023-11-20 14:28:26 +0100

    docs: Add listener-operator where missing (#533)
    * docs: Add listener-operator where missing
    
    * docs: Add listener-operator where missing

--- docs/modules/nifi/examples/getting_started/getting_started.sh
+++ docs/modules/nifi/examples/getting_started/getting_started.sh
@@ -32,6 +32,7 @@ echo "Installing Operators with Helm"
 # tag::helm-install-operators[]
 helm install --wait commons-operator stackable-dev/commons-operator --version 0.0.0-dev
 helm install --wait secret-operator stackable-dev/secret-operator --version 0.0.0-dev
+helm install --wait listener-operator stackable-dev/listener-operator --version 0.0.0-dev
 helm install --wait zookeeper-operator stackable-dev/zookeeper-operator --version 0.0.0-dev
 helm install --wait nifi-operator stackable-dev/nifi-operator --version 0.0.0-dev
 # end::helm-install-operators[]
@@ -42,6 +43,7 @@ echo "installing Operators with stackablectl"
 stackablectl operator install \
   commons=0.0.0-dev \
   secret=0.0.0-dev \
+  listener=0.0.0-dev \
   zookeeper=0.0.0-dev \
   nifi=0.0.0-dev
 # end::stackablectl-install-operators[]
