6ad07870678af9ae1949a0488e3c0c713e502d72
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2024-05-24 13:42:36 +0200

    Update rust/crd/src/tls.rs
    Co-authored-by: Nick <NickLarsenNZ@users.noreply.github.com>

--- rust/crd/src/tls.rs
+++ rust/crd/src/tls.rs
@@ -4,9 +4,9 @@ use stackable_operator::schemars::{self, JsonSchema};
 #[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct NifiTls {
-    /// Only affects client connections.
-    /// This setting controls:
-    /// - Which cert the servers should use to authenticate themselves against the client
+    /// This only affects client connections and is used to
+    /// control which certificate the servers should use to
+    /// authenticate themselves against the client.
     #[serde(default = "NifiTls::default_server_secret_class")]
     pub server_secret_class: String,
 }
