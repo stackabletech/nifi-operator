ec83a524a61160fa159afe924536184d5b1689d3
Benedikt Labrenz <benedikt@labrenz.org>
2024-07-31 16:56:46 +0200

    fix oidc test

--- tests/templates/kuttl/oidc/login.py
+++ tests/templates/kuttl/oidc/login.py
@@ -15,12 +15,12 @@ tls = os.environ["OIDC_USE_TLS"]
 
 session = requests.Session()
 
-nifi = f"test-nifi-node-default.{namespace}.svc.cluster.local"
+nifi = f"test-nifi-node-default-0.test-nifi-node-default.{namespace}.svc.cluster.local"
 keycloak_service = f"keycloak.{namespace}.svc.cluster.local"
 
 # Open NiFi web UI which will redirect to OIDC login
 login_page = session.get(
-    f"https://{nifi}:8443/nifi",
+    f"https://{nifi}:8443/nifi/login",
     verify=False,
     headers={"Content-type": "application/json"},
 )
@@ -29,20 +29,19 @@ keycloak_base_url = (
     if tls == "true"
     else f"http://{keycloak_service}:8080"
 )
-print(login_page.url)
 assert login_page.ok, "Redirection from NiFi to Keycloak failed"
-#assert login_page.url.startswith(
-#    f"{keycloak_base_url}/realms/test/protocol/openid-connect/auth?scope=openid+email+profile&response_type=code&redirect_uri=https%3A%2F%2F{nifi}%3A9088%2Fdruid-ext%2Fdruid-pac4j%2Fcallback&state="
-#), "Redirection to Keycloak expected"
+assert login_page.url.startswith(
+    f"{keycloak_base_url}/realms/test/protocol/openid-connect/auth?response_type=code&client_id=nifi&scope=openid%20email%20profile&state="
+), "Redirection to Keycloak expected"
 
 # Login to keycloak with test user
 login_page_html = BeautifulSoup(login_page.text, "html.parser")
 authenticate_url = login_page_html.form["action"]
 welcome_page = session.post(
-    authenticate_url, data={"username": "nifi-admin", "password": "nifi-admin"}
+    authenticate_url, data={"username": "jane.doe", "password": "T8mn72D9"}, verify=False
 )
 print(welcome_page.url)
 assert welcome_page.ok, "Login failed"
 assert (
-    welcome_page.url == f"https://{nifi}:8443/nifi"
+    welcome_page.url == f"https://{nifi}:8443/nifi/"
 ), "Redirection to the NiFi web UI expected"
