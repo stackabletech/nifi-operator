024744580fb307fc86ed6b5a452bcbaaf1a406e7
Benedikt Labrenz <benedikt@labrenz.org>
2024-08-06 22:39:16 +0200

    fix test logging

--- tests/templates/kuttl/oidc/login.py
+++ tests/templates/kuttl/oidc/login.py
@@ -29,7 +29,7 @@ keycloak_base_url = (
     if tls == "true"
     else f"http://{keycloak_service}:8080"
 )
-print("[login] found: ", login_page)
+print("[login] found: ", login_page.url)
 print("[login] expected: ", f"{keycloak_base_url}/realms/test/protocol/openid-connect/auth?response_type=code&client_id=nifi&scope=openid%20email%20profile&state=")
 assert login_page.ok, "Redirection from NiFi to Keycloak failed"
 assert login_page.url.startswith(
@@ -42,7 +42,7 @@ authenticate_url = login_page_html.form["action"]
 welcome_page = session.post(
     authenticate_url, data={"username": "jane.doe", "password": "T8mn72D9"}, verify=False
 )
-print("[redirect] found: ", welcome_page)
+print("[redirect] found: ", welcome_page.url)
 print("[redirect] expected: ", f"https://{nifi}:8443/nifi/")
 assert welcome_page.ok, "Login failed"
 assert (
