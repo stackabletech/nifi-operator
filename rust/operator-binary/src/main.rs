6114153f7aa36eac851509a632ecabff18300d5b
Nick Larsen <nick.larsen@stackable.tech>
2025-04-08 11:14:58 +0200

    feat!: Replace log initialization with stackable-telemetry
    BREAKING: The log directory variable for the rolling file appender has changed

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -1,10 +1,10 @@
-use std::sync::Arc;
+use std::{ops::Deref as _, sync::Arc};
 
 use clap::Parser;
 use futures::stream::StreamExt;
 use stackable_operator::{
     YamlSchema,
-    cli::{Command, ProductOperatorRun},
+    cli::{Command, ProductOperatorRun, RollingPeriod},
     commons::authentication::AuthenticationClass,
     k8s_openapi::api::{
         apps::v1::StatefulSet,
@@ -22,6 +22,8 @@ use stackable_operator::{
     logging::controller::report_controller_reconciled,
     shared::yaml::SerializeOptions,
 };
+use stackable_telemetry::{Tracing, tracing::settings::Settings};
+use tracing::level_filters::LevelFilter;
 
 use crate::{
     controller::NIFI_FULL_CONTROLLER_NAME,
@@ -42,6 +44,9 @@ mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
 
+// TODO (@NickLarsenNZ): Change the variable to `CONSOLE_LOG`
+pub const ENV_VAR_CONSOLE_LOG: &str = "NIFI_OPERATOR_LOG";
+
 #[derive(Parser)]
 #[clap(about, author)]
 struct Opts {
@@ -58,14 +63,51 @@ async fn main() -> anyhow::Result<()> {
         Command::Run(ProductOperatorRun {
             product_config,
             watch_namespace,
-            tracing_target,
+            telemetry_arguments,
             cluster_info_opts,
         }) => {
-            stackable_operator::logging::initialize_logging(
-                "NIFI_OPERATOR_LOG",
-                "nifi-operator",
-                tracing_target,
-            );
+            // NIFI_OPERATOR_LOG
+            // "nifi-operator",
+
+            let _tracing_guard = Tracing::builder()
+                // TODO (@NickLarsenNZ): Make this a constant
+                .service_name("nifi-operator")
+                .with_console_output((
+                    ENV_VAR_CONSOLE_LOG,
+                    LevelFilter::INFO,
+                    !telemetry_arguments.no_console_output,
+                ))
+                // NOTE (@NickLarsenNZ): Before stackable-telemetry was used, the log directory was
+                // set via an env: `NIFI_OPERATOR_LOG_DIRECTORY`.
+                // See: https://github.com/stackabletech/operator-rs/blob/f035997fca85a54238c8de895389cc50b4d421e2/crates/stackable-operator/src/logging/mod.rs#L40
+                // Now it will be `ROLLING_LOGS` (or via `--rolling-logs <DIRECTORY>`).
+                .with_file_output(telemetry_arguments.rolling_logs.map(|log_directory| {
+                    let rotation_period = telemetry_arguments
+                        .rolling_logs_period
+                        .unwrap_or(RollingPeriod::Never)
+                        .deref()
+                        .clone();
+
+                    Settings::builder()
+                        .with_environment_variable(ENV_VAR_CONSOLE_LOG)
+                        .with_default_level(LevelFilter::INFO)
+                        .file_log_settings_builder(log_directory, "tracing-rs.log")
+                        .with_rotation_period(rotation_period)
+                        .build()
+                }))
+                .with_otlp_log_exporter((
+                    "OTLP_LOG",
+                    LevelFilter::DEBUG,
+                    telemetry_arguments.otlp_logs,
+                ))
+                .with_otlp_trace_exporter((
+                    "OTLP_TRACE",
+                    LevelFilter::DEBUG,
+                    telemetry_arguments.otlp_traces,
+                ))
+                .build()
+                .init()?;
+
             tracing::info!(
                 built_info.pkg_version = built_info::PKG_VERSION,
                 built_info.git_version = built_info::GIT_VERSION,
