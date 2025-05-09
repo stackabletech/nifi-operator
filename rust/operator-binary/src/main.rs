13e60bc53138f83d6c6f120e5ef4c86050ea5349
Benedikt Labrenz <benedikt@labrenz.org>
2025-05-09 16:27:29 +0200

    refactor references_config_map function

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -179,12 +179,22 @@ fn references_config_map(
         return false;
     };
 
-    nifi.spec.cluster_config.zookeeper_config_map_name == config_map.name_any()
-        || match nifi.spec.cluster_config.authorization.to_owned() {
-            Some(trino_authorization) => match trino_authorization.opa {
-                Some(opa_config) => opa_config.opa.config_map_name == config_map.name_any(),
-                None => false,
-            },
-            None => false,
-        }
+    let references_zookeeper_config_map =
+        nifi.spec.cluster_config.zookeeper_config_map_name == config_map.name_any();
+    let references_authorization_config_map = references_authorization_config_map(nifi, config_map);
+
+    references_zookeeper_config_map || references_authorization_config_map
+}
+
+fn references_authorization_config_map(
+    nifi: &v1alpha1::NifiCluster,
+    config_map: &DeserializeGuard<ConfigMap>,
+) -> bool {
+    nifi.spec
+        .cluster_config
+        .authorization
+        .as_ref()
+        .and_then(|authz| authz.opa.as_ref())
+        .map(|opa_config| opa_config.opa.config_map_name == config_map.name_any())
+        .unwrap_or(false)
 }
