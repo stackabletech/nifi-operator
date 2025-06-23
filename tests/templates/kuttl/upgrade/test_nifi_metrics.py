3c7850f5fbf77171769edbd52d5ff9f2ebc69e6e
Benedikt Labrenz <benedikt@labrenz.org>
2025-06-23 16:26:54 +0200

    fix integration tests

--- tests/templates/kuttl/upgrade/test_nifi_metrics.py
+++ tests/templates/kuttl/upgrade/test_nifi_metrics.py
@@ -4,18 +4,34 @@ import requests
 import time
 from requests.exceptions import ConnectionError
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Construct an argument parser
     all_args = argparse.ArgumentParser()
     # Add arguments to the parser
-    all_args.add_argument("-m", "--metric", required=False, default="nifi_amount_bytes_read",
-                          help="The name of a certain metric to check")
-    all_args.add_argument("-n", "--namespace", required=True,
-                          help="The namespace the test is running in")
-    all_args.add_argument("-p", "--port", required=False, default="8081",
-                          help="The port where metrics are exposed")
-    all_args.add_argument("-t", "--timeout", required=False, default="120",
-                          help="The timeout in seconds to wait for the metrics port to be opened")
+    all_args.add_argument(
+        "-m",
+        "--metric",
+        required=False,
+        default="nifi_amount_bytes_read",
+        help="The name of a certain metric to check",
+    )
+    all_args.add_argument(
+        "-n", "--namespace", required=True, help="The namespace the test is running in"
+    )
+    all_args.add_argument(
+        "-p",
+        "--port",
+        required=False,
+        default="8081",
+        help="The port where metrics are exposed",
+    )
+    all_args.add_argument(
+        "-t",
+        "--timeout",
+        required=False,
+        default="120",
+        help="The timeout in seconds to wait for the metrics port to be opened",
+    )
 
     args = vars(all_args.parse_args())
     metric_name = args["metric"]
@@ -23,7 +39,7 @@ if __name__ == '__main__':
     port = args["port"]
     timeout = int(args["timeout"])
 
-    url = f"http://test-nifi-node-default-0.test-nifi-node-default.{namespace}.svc.cluster.local:{port}/metrics"
+    url = f"http://test-nifi-node-default-0.test-nifi-node-default-metrics.{namespace}.svc.cluster.local:{port}/metrics"
 
     # wait for 'timeout' seconds
     t_end = time.time() + timeout
@@ -35,7 +51,9 @@ if __name__ == '__main__':
                 print("Test metrics succeeded!")
                 exit(0)
             else:
-                print(f"Could not find metric [{metric_name}] in response:\n {response.text}")
+                print(
+                    f"Could not find metric [{metric_name}] in response:\n {response.text}"
+                )
                 time.sleep(timeout)
         except ConnectionError:
             # NewConnectionError is expected until metrics are available
