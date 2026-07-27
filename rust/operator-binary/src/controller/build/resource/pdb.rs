0a9f3f88cc5d31d95b291d74894d0837d21dca90
Andrew Kenworthy <andrew.kenworthy@stackable.tech>
2026-07-27 12:05:44 +0200

    use the role parameter via its From impl in build_pdb

--- rust/operator-binary/src/controller/build/resource/pdb.rs
+++ rust/operator-binary/src/controller/build/resource/pdb.rs
@@ -23,7 +23,7 @@ pub fn build_pdb(
     let pdb = pod_disruption_budget_builder_with_role(
         cluster,
         &product_name(),
-        &NifiRole::Node.into(),
+        &role.into(),
         &operator_name(),
         &controller_name(),
     )
